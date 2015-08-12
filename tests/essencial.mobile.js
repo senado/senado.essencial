008682c62c35383e22e6b43a00563f231dbf5bad
fabianonunes <fabiano@nunes.me>
2015-08-11 22:24:23 -0300

    Atualizar testes para nova fonte e novo menu do topo

--- tests/essencial.mobile.js
+++ tests/essencial.mobile.js
@@ -3,18 +3,18 @@
 casper.start(phantom.rootUrl)
 .then(function() {
     phantomcss.pathToTest = './';
-    this.mouse.click('.titulo-principal');
+    this.mouse.click('.PortalTopo-title');
     phantomcss.screenshot('body', 'body')
     phantomcss.screenshot('.NavGlobal', 'topo global')
     phantomcss.screenshot('.PortalTopo', 'topo portal')
     phantomcss.screenshot('.FooterMain', 'rodape global')
 })
 .then(function() {
-    this.mouse.click('.titulo-principal');
+    this.mouse.click('.PortalTopo-title')
     this.mouse.click('.NavGlobal-toggle')
     phantomcss.screenshot('.NavGlobal-dropdownMenu', 'menu portais aberto')
 }).then(function () {
-    this.mouse.click('.titulo-principal');
+    this.mouse.click('.PortalTopo-title');
     this.mouse.click('.PortalTopo-toggler--menu')
     this.wait(400, function () {
         phantomcss.screenshot('.Nav--principal', 'nav principal aberto')
