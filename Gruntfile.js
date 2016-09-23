5603671cfa79ac7a240b9d9862dd822d302afbac
fabianonunes <fabiano@nunes.me>
2016-09-23 13:10:01 -0300

    remover tarefa phantom

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,3 @@
-var path = require('path')
-
 module.exports = function (grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
@@ -116,7 +114,6 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-he')
   grunt.loadNpmTasks('grunt-uncss')
   grunt.loadNpmTasks('grunt-banner')
-  grunt.loadNpmTasks('grunt-phantomcss')
   grunt.loadNpmTasks('grunt-contrib-less')
   grunt.loadNpmTasks('grunt-contrib-copy')
   grunt.loadNpmTasks('grunt-contrib-clean')
