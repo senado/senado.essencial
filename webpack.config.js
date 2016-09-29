15a2f9f4d7cddae8e8b72412b557ab9a724c70d8
fabianonunes <fabiano@nunes.me>
2016-09-29 16:14:46 -0300

    incluir versão no html de saída

--- webpack.config.js
+++ webpack.config.js
@@ -38,6 +38,8 @@ module.exports = {
       test: /[.](pug|jade)$/, loader: 'pug'
     }, {
       test: /[.]yaml$/, loaders: ['json', 'yaml']
+    }, {
+      test: /[.]json$/, loader: 'json'
     }]
   },
 
