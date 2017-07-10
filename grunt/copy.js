26d9a965ccf5f043e2f5b818f45309af907713e7
fabianonunes <fabiano@nunes.me>
2017-07-10 19:30:17 -0300

    formatação

--- grunt/copy.js
+++ grunt/copy.js
@@ -1,13 +1,25 @@
 module.exports = {
   html: {
     files: [{
-      expand: true, flatten: true, src: ['<%= config.dist %>/fat/*.html'], dest: '<%= config.dist %>/fat/utf-8'
+      expand: true,
+      flatten: true,
+      src: ['<%= config.dist %>/fat/*.html'],
+      dest: '<%= config.dist %>/fat/utf-8'
     }, {
-      expand: true, flatten: true, src: ['<%= config.dist %>/fat/*.html'], dest: '<%= config.dist %>/iso-8859-1'
+      expand: true,
+      flatten: true,
+      src: ['<%= config.dist %>/fat/*.html'],
+      dest: '<%= config.dist %>/iso-8859-1'
     }, {
-      expand: true, flatten: true, src: ['<%= config.dist %>/thin/*.html'], dest: '<%= config.dist %>/thin/utf-8'
+      expand: true,
+      flatten: true,
+      src: ['<%= config.dist %>/thin/*.html'],
+      dest: '<%= config.dist %>/thin/utf-8'
     }, {
-      expand: true, flatten: true, src: ['<%= config.dist %>/thin/*.html'], dest: '<%= config.dist %>/iso-8859-1'
+      expand: true,
+      flatten: true,
+      src: ['<%= config.dist %>/thin/*.html'],
+      dest: '<%= config.dist %>/iso-8859-1'
     }]
   }
 }
